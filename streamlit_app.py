e64ad9cba8101de4b8300769b082e4d4f2c04113
SivaprasadDeepala <160743506+SivaprasadDeepala@users.noreply.github.com>
2024-02-22 14:26:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('my parents newly heartly dinner')
+streamlit.title('my parents newly Healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlt.text('Hard-Boiled Free-Range Egg')
+
